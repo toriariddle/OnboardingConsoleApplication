24309955961bf4e4469a50116c4d9890ae61c007
Tori Riddle <toriabsher@gmail.com>
2018-01-27 14:33:42 -0800

    Added PinNumber to User Class

--- OnboardingApp/User.cs
+++ OnboardingApp/User.cs
@@ -10,7 +10,8 @@ namespace OnboardingApp
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string FullName => string.Join(" " , FirstName , LastName);
+        public string FullName => string.Join(" ", FirstName, LastName);
         public bool IsAccountOwner { get; set; }
+        public int PinNumber { get; set; }
     }
 }
